67a5fac695920d411eb0a67e81e3ce0fe3aafff1
Kerim Turak <68472927+kerimturak@users.noreply.github.com>
2021-03-26 23:03:11 +0300

    Add files via upload

---
+++ Competition/Soru_yarismasi/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿
+namespace Soru_yarismasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnA = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btnB = new System.Windows.Forms.Button();
+            this.btnD = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblFalse = new System.Windows.Forms.Label();
+            this.lblTrue = new System.Windows.Forms.Label();
+            this.lblnumberque = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.correctAns = new System.Windows.Forms.Label();
+            this.answer = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(830, 488);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(62)))), ((int)(((byte)(149)))));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.richTextBox1.Location = new System.Drawing.Point(140, 261);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(542, 55);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // btnA
+            // 
+            this.btnA.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(62)))), ((int)(((byte)(149)))));
+            this.btnA.FlatAppearance.BorderSize = 0;
+            this.btnA.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnA.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnA.Location = new System.Drawing.Point(128, 346);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(255, 52);
+            this.btnA.TabIndex = 2;
+            this.btnA.Text = "A";
+            this.btnA.UseVisualStyleBackColor = false;
+            this.btnA.Click += new System.EventHandler(this.btnA_Click);
+            // 
+            // btnC
+            // 
+            this.btnC.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(62)))), ((int)(((byte)(149)))));
+            this.btnC.FlatAppearance.BorderSize = 0;
+            this.btnC.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnC.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnC.Location = new System.Drawing.Point(128, 411);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(255, 50);
+            this.btnC.TabIndex = 3;
+            this.btnC.Text = "C";
+            this.btnC.UseVisualStyleBackColor = false;
+            this.btnC.Click += new System.EventHandler(this.btnC_Click);
+            // 
+            // btnB
+            // 
+            this.btnB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(62)))), ((int)(((byte)(149)))));
+            this.btnB.FlatAppearance.BorderSize = 0;
+            this.btnB.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnB.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnB.Location = new System.Drawing.Point(474, 346);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(263, 52);
+            this.btnB.TabIndex = 4;
+            this.btnB.Text = "B";
+            this.btnB.UseVisualStyleBackColor = false;
+            this.btnB.Click += new System.EventHandler(this.btnB_Click);
+            // 
+            // btnD
+            // 
+            this.btnD.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(62)))), ((int)(((byte)(149)))));
+            this.btnD.FlatAppearance.BorderSize = 0;
+            this.btnD.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnD.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnD.Location = new System.Drawing.Point(474, 411);
+            this.btnD.Name = "btnD";
+            this.btnD.Size = new System.Drawing.Size(263, 50);
+            this.btnD.TabIndex = 5;
+            this.btnD.Text = "D";
+            this.btnD.UseVisualStyleBackColor = false;
+            this.btnD.Click += new System.EventHandler(this.btnD_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(326, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(181, 209);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(326, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(181, 209);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(62)))), ((int)(((byte)(149)))));
+            this.groupBox1.Controls.Add(this.lblFalse);
+            this.groupBox1.Controls.Add(this.lblTrue);
+            this.groupBox1.Controls.Add(this.lblnumberque);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(608, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 126);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Score";
+            // 
+            // lblFalse
+            // 
+            this.lblFalse.AutoSize = true;
+            this.lblFalse.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFalse.Location = new System.Drawing.Point(113, 82);
+            this.lblFalse.Name = "lblFalse";
+            this.lblFalse.Size = new System.Drawing.Size(16, 16);
+            this.lblFalse.TabIndex = 14;
+            this.lblFalse.Text = "0";
+            // 
+            // lblTrue
+            // 
+            this.lblTrue.AutoSize = true;
+            this.lblTrue.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTrue.Location = new System.Drawing.Point(113, 57);
+            this.lblTrue.Name = "lblTrue";
+            this.lblTrue.Size = new System.Drawing.Size(16, 16);
+            this.lblTrue.TabIndex = 13;
+            this.lblTrue.Text = "0";
+            this.lblTrue.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // lblnumberque
+            // 
+            this.lblnumberque.AutoSize = true;
+            this.lblnumberque.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnumberque.Location = new System.Drawing.Point(113, 30);
+            this.lblnumberque.Name = "lblnumberque";
+            this.lblnumberque.Size = new System.Drawing.Size(16, 16);
+            this.lblnumberque.TabIndex = 12;
+            this.lblnumberque.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(40, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "False :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(47, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "True :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Q. Number :";
+            // 
+            // correctAns
+            // 
+            this.correctAns.AutoSize = true;
+            this.correctAns.Location = new System.Drawing.Point(12, 9);
+            this.correctAns.Name = "correctAns";
+            this.correctAns.Size = new System.Drawing.Size(35, 13);
+            this.correctAns.TabIndex = 9;
+            this.correctAns.Text = "label4";
+            this.correctAns.Visible = false;
+            // 
+            // answer
+            // 
+            this.answer.AutoSize = true;
+            this.answer.Location = new System.Drawing.Point(12, 31);
+            this.answer.Name = "answer";
+            this.answer.Size = new System.Drawing.Size(35, 13);
+            this.answer.TabIndex = 10;
+            this.answer.Text = "label5";
+            this.answer.Visible = false;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNext.Location = new System.Drawing.Point(736, 261);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(82, 41);
+            this.btnNext.TabIndex = 11;
+            this.btnNext.Text = "NEXT";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 488);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.answer);
+            this.Controls.Add(this.correctAns);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnD);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btnA);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnA;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Button btnB;
+        private System.Windows.Forms.Button btnD;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblFalse;
+        private System.Windows.Forms.Label lblTrue;
+        private System.Windows.Forms.Label lblnumberque;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label correctAns;
+        private System.Windows.Forms.Label answer;
+        private System.Windows.Forms.Button btnNext;
+    }
+}
+
---
+++ Competition/Soru_yarismasi/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soru_yarismasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int numberque = 0, ans_corr = 0, ans_wrng = 0;
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnA_Click(object sender, EventArgs e)
+        {
+            btnA.Enabled = false;
+            btnB.Enabled = false;
+            btnC.Enabled = false;
+            btnD.Enabled = false;
+            btnNext.Enabled = true;
+
+            answer.Text = btnA.Text;
+
+            if(correctAns.Text == answer.Text)
+            {
+                ans_corr++;
+                pictureBox2.Visible = true;
+                lblTrue.Text = ans_corr.ToString();
+            }
+            else
+            {
+                ans_wrng++;
+                pictureBox3.Visible = true;
+                lblFalse.Text = ans_wrng.ToString();
+            }
+        }
+
+        private void btnB_Click(object sender, EventArgs e)
+        {
+            btnA.Enabled = false;
+            btnB.Enabled = false;
+            btnC.Enabled = false;
+            btnD.Enabled = false;
+            btnNext.Enabled = true;
+
+            answer.Text = btnB.Text;
+
+            if (correctAns.Text == answer.Text)
+            {
+                ans_corr++;
+                pictureBox2.Visible = true;
+                lblTrue.Text = ans_corr.ToString();
+            }
+            else
+            {
+                ans_wrng++;
+                pictureBox3.Visible = true;
+                lblFalse.Text = ans_wrng.ToString();
+            }
+        }
+
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            btnA.Enabled = false;
+            btnB.Enabled = false;
+            btnC.Enabled = false;
+            btnD.Enabled = false;
+            btnNext.Enabled = true;
+
+            answer.Text = btnC.Text;
+
+            if (correctAns.Text == answer.Text)
+            {
+                ans_corr++;
+                pictureBox2.Visible = true;
+                lblTrue.Text = ans_corr.ToString();
+            }
+            else
+            {
+                ans_wrng++;
+                pictureBox3.Visible = true;
+                lblFalse.Text = ans_wrng.ToString();
+            }
+        }
+
+        private void btnD_Click(object sender, EventArgs e)
+        {
+            btnA.Enabled = false;
+            btnB.Enabled = false;
+            btnC.Enabled = false;
+            btnD.Enabled = false;
+            btnNext.Enabled = true;
+
+            answer.Text = btnD.Text;
+
+            if (correctAns.Text == answer.Text)
+            {
+                ans_corr++;
+                pictureBox2.Visible = true;
+                lblTrue.Text = ans_corr.ToString();
+            }
+            else
+            {
+                ans_wrng++;
+                pictureBox3.Visible = true;
+                lblFalse.Text = ans_wrng.ToString();
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            btnA.Enabled = true;
+            btnB.Enabled = true;
+            btnC.Enabled = true;
+            btnD.Enabled = true;
+            btnNext.Enabled = false;
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+
+            numberque++;
+            lblnumberque.Text = numberque.ToString();
+
+            if(numberque == 1)
+            {
+                richTextBox1.Text = "Where did Mehmet Akif write the Turkish National Anthem??";
+                btnA.Text = "Ankara - Ayasofya";
+                btnB.Text = "Ankara - Keçiören Camisi";
+                btnC.Text = "Ankara - Ayaş Dergahı";
+                btnD.Text = "Ankara - Taceddin Dergahı";
+                correctAns.Text = "Ankara - Taceddin Dergahı";
+            }
+
+            if(numberque == 2)
+            {
+                richTextBox1.Text = "Atatürk'ün yurt gezilerinde (1935-1938) yılları arasında kullandığı trenin adı nedir?";
+                btnA.Text = "Beyaz Tren";
+                btnB.Text = "Kara Tren";
+                btnC.Text = "Demir Türk";
+                btnD.Text = "Anadolu";
+                correctAns.Text = "Beyaz Tren";
+            }
+
+            if (numberque == 3)
+            {
+                richTextBox1.Text = "Türkiye'nin ilk Safari Parkı hangi ilimizde açılmıştır?";
+                btnA.Text = "Ankara";
+                btnB.Text = "İzmir";
+                btnC.Text = "Gaziantep";
+                btnD.Text = "Manisa";
+                correctAns.Text = "Gaziantep";
+            }
+
+            if (numberque == 4)
+            {
+                richTextBox1.Text = "Birleşmiş Milletler Dünya Turizm Örgütü'nün kısa adı hangisidir?";
+                btnA.Text = "UNWTO";
+                btnB.Text = "WTO";
+                btnC.Text = "UWT";
+                btnD.Text = "UNOWT";
+                correctAns.Text = "UNWTO";
+                btnNext.Text = "Sonuçlar";
+            }
+
+            if (numberque == 5)
+            {
+                btnA.Enabled = false;
+                btnB.Enabled = false;
+                btnC.Enabled = false;
+                btnD.Enabled = false;
+                btnNext.Enabled = false;
+                MessageBox.Show("Doğru: " + ans_corr + "\n" + "Yanlış: " + ans_wrng);
+            }
+        }
+    }
+}
